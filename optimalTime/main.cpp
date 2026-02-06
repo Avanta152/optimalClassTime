29ae4e3a16522dff5f42ed50c9d4322b42dcd260
Avanta152 <s-anna.gaisina@lwtech.edu>
2026-02-06 13:09:20 -0800

    second
    develop a function for storing names

--- optimalTime/main.cpp
+++ optimalTime/main.cpp
@@ -2,9 +2,31 @@
 #include <fstream>
 #include <string>
 
-
 using namespace std;
+
+struct ListNode {
+    string name;
+    ListNode* next;
+};
+void insertName(ListNode*& head, string& student) {
+    ListNode* listNode = new ListNode;
+    listNode->name = student;
+    listNode->next = head;
+    head = listNode;
+}
+void printList(ListNode* head) {
+    ListNode* ptr = head;
+    while (ptr) {
+        cout << ptr->name << " ";
+        ptr = ptr->next;
+    }
+}
+
+
+
 int main(int argc , char *argv[]) {
+
+    ListNode*nameList = NULL;
 
     for (int i = 1; i< argc; i++) {
         ifstream file(argv[i]); 
@@ -15,11 +37,16 @@ int main(int argc , char *argv[]) {
         }
         cout << "Opened file: " << argv[i] << endl;
         string line;
+        string name = argv[i];
+        insertName(nameList, name);
         while (getline(file, line)) {
-cout << line<< endl;
+            
+    cout << line<< endl;
         }
 file.close();
         }
+        printList(nameList);
+        cout << endl;
     
 
 
