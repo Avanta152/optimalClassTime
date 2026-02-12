b65a4cbd5691e45f588c6bceb6eca4c885290ffb
Avanta152 <s-anna.gaisina@lwtech.edu>
2026-02-12 13:58:56 -0800

    final changes
    created a user input loop to ask how many students picked the same time and functions to clean the memory, fixed the TimeNode list to store days.

--- optimalTime/main.cpp
+++ optimalTime/main.cpp
@@ -4,17 +4,19 @@
 
 using namespace std;
 
+//node to store the names
 struct ListNode {
     string name;
     ListNode* next;
 };
+//function to insert manes into a names list
 void insertName(ListNode*& head, string& student) {
     ListNode* listNode = new ListNode;
     listNode->name = student;
     listNode->next = head;
     head = listNode;
 }
-
+//function to print the list
 void printList(ListNode* head) {
     ListNode* ptr = head;
     while (ptr) {
@@ -22,40 +24,26 @@ void printList(ListNode* head) {
         ptr = ptr->next;
     }
 }
+//list to store days. times, number of students, that points to the name list
 struct TimeNode {
+    string day;
     double time;
     int count;
     ListNode*nameList;
     TimeNode* next;
 };
-TimeNode* insertTime(TimeNode*& head, double t, string& name) {
-    if (!head) {
-        TimeNode* node = new TimeNode;
-        node->time = t;
-        node->count = 1;
-        node->nameList = nullptr;
-        insertName(node->nameList, name);
-        node->next= head;
-        return node;
-    }
-
-    if (t< head->time) {
-        TimeNode* node = new TimeNode;
-        node->time = t;
-        node->count = 1;
-        node->nameList = nullptr;
-        insertName(node->nameList, name);
-        node->next= head;
-        return node;
-    }
+//function that inserts the days, times ans names, counts how many students pick the same time
+TimeNode* insertTime(TimeNode*& head, string day, double t, string& name) {
+    
     TimeNode* ptr = head;
     TimeNode* temp = nullptr;
-    while (ptr && ptr->time<t) {
+    while (ptr && (ptr->day<day || (ptr->day == day && ptr->time<t))) 
+    {
         temp = ptr;
         ptr=ptr->next;
     }
     //if name exists in the list
-    if (ptr && ptr->time==t) {
+    if (ptr && ptr->day == day && ptr->time==t) {
         ListNode* check = ptr->nameList;
         bool found = false;
         while (check) {
@@ -66,26 +54,40 @@ TimeNode* insertTime(TimeNode*& head, double t, string& name) {
             check=check->next;
         }
         if (!found) {
-
         insertName(ptr->nameList, name);
-
         ptr->count++;
         }
         return head;
     }
+    //if name does not exist , creates a new node
     TimeNode* node = new TimeNode;
+    node->day = day;
         node->time = t;
         node->count = 1;
         node->nameList = nullptr;
         insertName(node->nameList, name);
         node->next= ptr;
-        if (temp) {
-            temp->next = node;
-        }
+        if (temp==NULL) {
+            head = node;
+        } else {temp->next = node;}
         return head;
-
 }
 
+void deleteNameList(ListNode*& head) {
+    while (head) {
+        ListNode*temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+void deleteTimeNode(TimeNode*& head) {
+    while (head) {
+        TimeNode* temp = head;
+        deleteNameList(head->nameList);
+        head=head->next;
+        delete temp;
+    }
+}
 int main(int argc , char *argv[]) {
 
     ListNode*nameList = NULL;
@@ -95,29 +97,38 @@ int main(int argc , char *argv[]) {
         ifstream file(argv[i]); 
 
         if (!file) {
-            cout<< "Could not open filr!" << argv[i] << endl;
+            cout<< "Could not open file!" << argv[i] << endl;
             continue;
         }
-        cout << "Opened file: " << argv[i] << endl;
-        
         string name = argv[i];
         insertName(nameList, name);
-        double time;
         string line;
         while (getline(file, line)) {
+            string day = line.substr(0, line.find(':'));
         double time = stod(line.substr(line.find(':') +1));
-            head = insertTime(head, time, name);
+            head = insertTime(head,day, time, name);
         }
         file.close();
         }
+        int minimum;
+        while (true) {
+            cout << "Enter the minimum number of stidents that must have chosen the same start time or 0 to exit-->";
+            cin >> minimum;
+            if (minimum == 0) {
+                cout <<"You exit."<< endl;
+                break;
+            }
         TimeNode*ptr = head;
         while (ptr ) {
-            cout << "time: " << ptr->time << " count: " << ptr->count << " names: " ;
+            if (ptr->count >= minimum) {
+            cout << "day: " << ptr->day<< " time: " << ptr->time << " names: " ;
             printList(ptr->nameList);
             cout<< endl;
+            }
             ptr = ptr-> next;
         }
-  
-
+    }
+    deleteTimeNode(head);
+    deleteNameList(nameList);
     return 0;
 }
